37412862f567872dbfa6ba2287b318973d50b197
faizan6xyz <232054644+faizan6xyz@users.noreply.github.com>
2026-08-18 06:03:16 +0530

    schedular

--- Instagram/schedule.py
+++ Instagram/schedule.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import time
-import upload as aaaa
+import Instagram.upload as aaaa
 import sqlite3
 
 DB = "schedule.db"
@@ -12,23 +12,27 @@ def init_db():
     conn = get_conn()
     conn.execute(""" CREATE TABLE IF NOT EXISTS schedule (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    user_id INTEGER NOT NULL,
+                    user_id TEXT NOT NULL,
                     time TEXT NOT NULL,
                     container_id TEXT NOT NULL,
                     access_token TEXT NOT NULL) """)
     conn.commit()
     conn.close()
 
-def insert_time(user_id, container_id, scheduled_time, access_token):
+def insert_time(user_id, container_id, scheduled_time, access_token):    # time should be give in the isoformat iniitally as argument 
     conn = get_conn()
     conn.execute(
-        "INSERT INTO schedule (user_id, container_id, time, access_token) VALUES (?, ?, ?, ?)",(user_id, container_id, scheduled_time.isoformat(), access_token))
+        "INSERT INTO schedule (user_id, container_id, time, access_token) VALUES (?, ?, ?, ?)",
+        (user_id, container_id, scheduled_time, access_token)
+    )
     conn.commit()
     conn.close()
 
 def get_containers_due(now):
     conn = get_conn()
-    cur = conn.execute("SELECT id, container_id, access_token, user_id FROM schedule WHERE time < ?", (now,))
+    cur = conn.execute(
+        "SELECT id, container_id, access_token, user_id FROM schedule WHERE time < ?", (now,)
+    )
     rows = cur.fetchall()
     conn.close()
     return rows
@@ -40,13 +44,15 @@ def delete_by_id(row_id):
     conn.close()
 
 init_db()
+# timmm = (datetime.now(timezone.utc) + timedelta(seconds=5)).isoformat()
+# insert_time("sdidfbdzdf", "fgfgfd", timmm, "dsajhbdhf")
 
 while True:
     now = datetime.now(timezone.utc).isoformat()
     due = get_containers_due(now)
-
     for row_id, container_id, access_tok, user_id in due:
         aaaa.publish_container(user_id=user_id, access_token=access_tok, creation_id=container_id)
+        # print(row_id,container_id,access_tok,user_id)
         delete_by_id(row_id)
 
     time.sleep(0.5)
