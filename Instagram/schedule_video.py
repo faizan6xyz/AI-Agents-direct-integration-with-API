73f352ab4fee4b16fb876f6ca77a1d9c1d67d2ff
faizan6xyz <232054644+faizan6xyz@users.noreply.github.com>
2026-08-18 12:09:30 +0530

    made this

--- Instagram/schedule_video.py
+++ Instagram/schedule_video.py
@@ -21,18 +21,13 @@ def init_db():
 
 def insert_time(user_id, container_id, scheduled_time, access_token):    # time should be give in the isoformat iniitally as argument 
     conn = get_conn()
-    conn.execute(
-        "INSERT INTO schedule (user_id, container_id, time, access_token) VALUES (?, ?, ?, ?)",
-        (user_id, container_id, scheduled_time, access_token)
-    )
+    conn.execute("INSERT INTO schedule (user_id, container_id, time, access_token) VALUES (?, ?, ?, ?)",(user_id, container_id, scheduled_time, access_token))
     conn.commit()
     conn.close()
 
 def get_containers_due(now):
     conn = get_conn()
-    cur = conn.execute(
-        "SELECT id, container_id, access_token, user_id FROM schedule WHERE time < ?", (now,)
-    )
+    cur = conn.execute("SELECT id, container_id, access_token, user_id FROM schedule WHERE time < ?", (now,))
     rows = cur.fetchall()
     conn.close()
     return rows
@@ -43,16 +38,16 @@ def delete_by_id(row_id):
     conn.commit()
     conn.close()
 
-init_db()
+# init_db()
 # timmm = (datetime.now(timezone.utc) + timedelta(seconds=5)).isoformat()
 # insert_time("sdidfbdzdf", "fgfgfd", timmm, "dsajhbdhf")
 
-while True:
-    now = datetime.now(timezone.utc).isoformat()
-    due = get_containers_due(now)
-    for row_id, container_id, access_tok, user_id in due:
-        aaaa.publish_container(user_id=user_id, access_token=access_tok, creation_id=container_id)
-        # print(row_id,container_id,access_tok,user_id)
-        delete_by_id(row_id)
-
-    time.sleep(0.5)
+if __name__ == "__main__":
+    while True:
+        now = datetime.now(timezone.utc).isoformat()
+        due = get_containers_due(now)
+        for row_id, container_id, access_tok, user_id in due:
+            # aaaa.publish_container(user_id=user_id, access_token=access_tok, creation_id=container_id)
+            print(row_id,container_id,access_tok,user_id)
+            delete_by_id(row_id)
+        time.sleep(1)
